f4cd16dfb704c62ff8e84792ed768ca33f566200
Richardson <104580727+codewithrich@users.noreply.github.com>
2023-07-15 21:15:46 +0530

    Update index.js with additional statements

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 parota = prompt("Enter the number of parottas ate: ");
 const soori = new Promise((resolve,reject) => {
     if (parota==50){
-        resolve("50 saptachu soori.. congratulations") }
-    else {
+        resolve("50 saptachu soori.. congratulations")}
+    else  if(parota>50){
+        resolve("Hey you ate more than 50 da soori")}
+    else{
         reject("Ella kotaiyum alinga first la irundhu sapudren")}
 })
 
